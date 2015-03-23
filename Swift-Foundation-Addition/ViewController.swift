59f89d846c3ca7da60a9dbf52aff5864f19ade67
Vincent Bernier <vincentbernier@hotmail.com>
2015-03-22 22:41:50 -0400

    Add a Xcode project to work on the development

---
+++ Swift-Foundation-Addition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Foundation-Addition
+//
+//  Created by Vincent Bernier on 22-03-2015.
+//  Copyright (c) 2015 Vincent Bernier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
