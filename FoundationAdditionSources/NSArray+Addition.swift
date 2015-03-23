01398b2aa55f90fc6261888dc000258b850a2dcf
Vincent Bernier <vincentbernier@hotmail.com>
2015-03-22 23:05:29 -0400

    add a pod spec file, with basic information add the first swift file delete the temporary txt file

---
+++ FoundationAdditionSources/NSArray+Addition.swift
@@ -0,0 +1,11 @@
+//
+//  NSArray+Addition.swift
+//  Swift-Foundation-Addition
+//
+//  Created by Vincent Bernier on 22-03-2015.
+//  Copyright (c) 2015 Vincent Bernier. All rights reserved.
+//
+
+import Foundation
+
+
