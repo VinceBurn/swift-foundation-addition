5bad8d64ae6820c7919aaac8f917236cd2859f8a
Vincent Bernier <vincentbernier@hotmail.com>
2015-04-18 14:05:54 -0400

    Add a WeakWrapper class

---
+++ FoundationAdditionSources/WeakWrapper.swift
@@ -0,0 +1,13 @@
+//
+//  WeakWrapper.swift
+//  Swift-Foundation-Addition
+//
+//  Created by Vincent Bernier on 18-04-2015.
+//  Copyright (c) 2015 Vincent Bernier. All rights reserved.
+//
+
+import Foundation
+
+public class WeakWrapper {
+    public init() {}
+}
---
+++ Swift-Foundation-AdditionTests/WeakWrapper_test.swift
@@ -0,0 +1,18 @@
+//
+//  WeakWrapper_test.swift
+//  Swift-Foundation-Addition
+//
+//  Created by Vincent Bernier on 18-04-2015.
+//  Copyright (c) 2015 Vincent Bernier. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class WeakWrapper_test: XCTestCase {
+    
+    func test_existance() {
+        let x = WeakWrapper()
+    }
+
+}
